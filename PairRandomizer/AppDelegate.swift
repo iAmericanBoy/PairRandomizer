6fa301a6eba625b5170b2bcb6d7c48659a7973b5
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-08 09:08:16 -0700

    Inital Commit

---
+++ PairRandomizer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PairRandomizer
+//
+//  Created by Dominic Lanzillotta on 3/8/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
